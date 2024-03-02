c08ac4041534ce0f6632d57512c18e8ee1b5d3b2
P0povaI <123860481+P0povaI@users.noreply.github.com>
2024-03-02 13:24:50 +0200

    Add files via upload

---
+++ CarManagementApp/CarManagementApp/Models/Client.cs
@@ -0,0 +1,10 @@
+﻿namespace CarManagementApp.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Birthday { get; set;}
+    }
+}
